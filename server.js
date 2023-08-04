307d53a510f3d9c7fc56cb65b2e8cfcfe0cf6abd
tyler ferre <ferre.tyler1@gmail.com>
2023-08-04 13:57:16 -0600

    updates

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const path = require('path')
 
 app.use(express.json())
 app.use(morgan('dev'))
-app.use(express.static(path.join(__dirname, "client", "dist")))
+app.use(express.static(path.join(__dirname, "client", "build")))
 
 mongoose.connect(
     'mongodb://localhost:27017/nature-hub-db',
@@ -30,7 +30,7 @@ app.use((err, req, res, next) => {
 })
 
 app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "client", "build", "index.html"))
+    res.sendFile(path.join(__dirname, "client", "dist", "index.html"))
 })
 
 app.listen(9000, () => {
